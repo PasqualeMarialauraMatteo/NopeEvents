4cd2c94a468a2e5707334ef3daaf8bbf3850553d
Pasquale Leonardo Lazzaro <pas.lazzaro@stud.uniroma3.it>
2022-06-21 09:49:57 +0200

    .

--- nope-events/src/main/java/it/nopeevents/football/controller/SquadraController.java
+++ nope-events/src/main/java/it/nopeevents/football/controller/SquadraController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import it.nopeevents.football.model.Squadra;
@@ -55,5 +56,4 @@ public class SquadraController {
 		model.addAttribute("listTornei", listTornei);
 		return "squadra/squadraForm.html";
 	}
-
 }
