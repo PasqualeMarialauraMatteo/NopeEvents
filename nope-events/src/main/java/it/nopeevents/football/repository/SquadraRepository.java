b60146680dee697607904a10a0cd5599989b0d86
MM <menicacci@outlook.com>
2022-06-21 11:05:55 +0200

    import

--- nope-events/src/main/java/it/nopeevents/football/repository/SquadraRepository.java
+++ nope-events/src/main/java/it/nopeevents/football/repository/SquadraRepository.java
@@ -1,14 +1,10 @@
 package it.nopeevents.football.repository;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 
 import it.nopeevents.football.model.Squadra;
 
 public interface SquadraRepository extends CrudRepository<Squadra,Long> {
 
-	boolean existsByNome(String nome);
-
-	
+	public boolean existsByNome(String nome);
 }
--- nope-events/src/main/java/it/nopeevents/football/repository/TorneoRepository.java
+++ nope-events/src/main/java/it/nopeevents/football/repository/TorneoRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 import it.nopeevents.football.model.Torneo;
 
 public interface TorneoRepository extends CrudRepository<Torneo,Long> {
+	
 	public boolean existsByNomeAndDescrizioneAndDataInizio(String nome,String descrizione, LocalDate dataInizio);
 }
